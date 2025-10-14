4836e696dc24ed8b4420821538160d8b03f95174
Phyu Sin Shin Thant <phyusinshinthant02@gmail.com>
2025-10-14 08:50:33 +0800

    Update app.py

--- project/app.py
+++ project/app.py
@@ -26,7 +26,7 @@ app.config["SESSION_PERMANENT"] = False
 #app.config["SESSION_TYPE"] = "filesystem"
 #Session(app)
 # Use default Flask session (signed cookies)
-app.config["SESSION_COOKIE_SECURE"] = True     # signed cookies
+#app.config["SESSION_COOKIE_SECURE"] = True     # signed cookies
 app.config["SESSION_COOKIE_SAMESITE"] = "Lax"  # Allows OAuth redirects
 
 oauth.init_app(app)  # Sets up Authlib OAuth with Flask
