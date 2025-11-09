1be917973ab72834562a04cb6992990d52a25a3c
Phyu Sin Shin Thant <phyusinshinthant02@gmail.com>
2025-11-09 13:23:19 +0800

    Update system_check.py

--- project/system_check.py
+++ project/system_check.py
@@ -3,3 +3,7 @@ import uuid
 from datetime import datetime, date, timedelta
 from flask import Blueprint, render_template, request, redirect, session, flash, url_for
 from helpers import login_required, show_error, get_db, choose_activities, removal_check, responses_check
+
+def remove_event():
+
+  
